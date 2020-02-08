c889614717eb5d4bfd9679535bae93ce15fb716f
Marijn Ophorst <marijn@monomelodies.nl>
2020-02-08 16:55:48 +0100

    the base report class

---
+++ src/Report.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Monomelodies\Codein;
+
+use ReflectionClass;
+use Reflector;
+
+abstract class Report
+{
+    /** @var string */
+    protected $message;
+
+    /** @var ReflectionClass */
+    private $reflection;
+
+    /** @var Reflector */
+    private $reflected;
+
+    /**
+     * @param ReflectionClass $reflection
+     * @param Reflector|null $reflected
+     * @return void
+     */
+    public function __construct(ReflectionClass $reflection, Reflector $reflected = null)
+    {
+        $this->reflection = $reflection;
+        $this->reflected = $reflected;
+    }
+
+    /**
+     * Returns a message with placeholders replaced.
+     *
+     * @return string
+     */
+    public function getMessage() : string
+    {
+        return str_replace(
+            ['{file}', '{line}', '{class}', '{method}', '{property}'],
+            [
+                $this->reflection->getFile(),
+                $this->reflected->getLine(),
+                $this->reflection->name,
+                isset($this->reflected) ? $this->reflected->getName() : '',
+                isset($this->reflected) ? $this->reflected->getName() : '',
+            ],
+            $this->message
+        );
+    }
+}
+
